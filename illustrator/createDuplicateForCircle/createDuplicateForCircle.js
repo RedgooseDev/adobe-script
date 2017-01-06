396cb12a4de91225f0345300c71b9109b7c1cd85
redgoose <scripter@me.com>
2017-01-06 19:34:59 +0900

    Add files

---
+++ illustrator/createDuplicateForCircle/createDuplicateForCircle.js
@@ -0,0 +1,147 @@
+// tested by Adobe Illustrator CC 2017
+
+
+// creating a document
+var doc = null;
+if ( app.documents && app.documents.length > 0 ) {
+    doc = app.activeDocument;
+} else {
+    doc = app.documents.add();
+}
+
+// set artboard
+var artboard = getActiveArtboard();
+
+
+// get active artboard
+function getActiveArtboard()
+{
+    var artboard = doc.artboards[doc.artboards.getActiveArtboardIndex()];
+    var result = {};
+    var ab = {};
+
+    ab.left = Math.round(artboard.artboardRect[0]);
+    ab.leftTop = Math.round(artboard.artboardRect[1]);
+    ab.right = Math.round(artboard.artboardRect[2]);
+    ab.rightBottom = Math.round(artboard.artboardRect[3]);
+
+    result.x = ab.left;
+    result.y = -ab.leftTop;
+    result.width = ab.right - ab.left;
+    result.height = ab.leftTop - ab.rightBottom;
+
+    return result;
+}
+
+
+// hex code to rgb
+function hexToRgb(hex)
+{
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+
+// create duplicate for circle
+function createDuplicateForCircle(options)
+{
+    var group = doc.groupItems.add();
+
+    for (var j = 0; j < options.countForCircle; j++)
+    {
+        var subgroup = group.groupItems.add();
+
+        for (i = 0; i < options.countForItem; i++)
+        {
+            var ray = null;
+
+            // set shape
+            switch(options.shape) {
+                case 'circle':
+                    ray = subgroup.pathItems.ellipse(0, 0, options.size, options.size);
+                    break;
+                case 'line':
+                    ray = subgroup.pathItems.add();
+                    ray.setEntirePath([ [0, 0], [0, options.size]]);
+                    break;
+                case 'rectangle':
+                    ray = subgroup.pathItems.rectangle(0, 0, options.size, options.size);
+                    break;
+            }
+
+            // color
+            var fillColorRef = new RGBColor;
+            fillColorRef.red = hexToRgb(options.fillColor).r;
+            fillColorRef.green = hexToRgb(options.fillColor).g;
+            fillColorRef.blue = hexToRgb(options.fillColor).b;
+            ray.fillColor = fillColorRef;
+
+            // border
+            if (options.strokeColor && (options.strokeWidth > 0))
+            {
+                var borderColorRef = new RGBColor;
+                borderColorRef.red = hexToRgb(options.strokeColor).r;
+                borderColorRef.green = hexToRgb(options.strokeColor).g;
+                borderColorRef.blue = hexToRgb(options.strokeColor).b;
+                ray.strokeColor = borderColorRef;
+            }
+            else
+            {
+                var noColor = new NoColor();
+                ray.strokeColor = noColor;
+            }
+            ray.strokeWidth = (options.strokeWidth > 0) ? options.strokeWidth : 0;
+
+            // rotate shape
+            switch(options.rotateMathod) {
+                case 'random':
+                    ray.rotation = Math.random() * 360;
+                    break;
+                default:
+                    ray.rotation = (360 / options.countForItem) * i;
+                    break;
+            }
+            ray.rotate(ray.rotation, true, true, true, true, Transformation.BOTTOM);
+
+            // set margin
+            var displacement = (options.size + options.offset) * (j + 1) + options.innerOffset;
+
+            var dx =  displacement * Math.sin( (180 + ray.rotation) * Math.PI / 180 );
+            var dy =- displacement * Math.cos( (180 + ray.rotation) * Math.PI / 180 );
+
+            ray.translate(dx, dy);
+        }
+    }
+
+    // change position
+    if (options.groupPosition && options.groupPosition.constructor == Array)
+    {
+        group.left = options.groupPosition[0];
+        group.top = -options.groupPosition[1];
+    }
+    else
+    {
+        group.left = Math.round((artboard.width * 0.5) - (group.width * 0.5) + artboard.x);
+        group.top = Math.round((group.height * 0.5) - (artboard.height * 0.5) - artboard.y);
+    }
+}
+
+
+// act func
+createDuplicateForCircle({
+    groupPosition : null, // [10, 10], null
+    countForItem : 32,
+    countForCircle : 5,
+    offset: 2,
+    innerOffset: 40,
+    rotateMathod: null, // random, null
+    fillColor: '#ff0000',
+    shape: 'rectangle', // line, rectangle, circle
+    size: 5,
+    strokeWidth : 0,
+    strokeColor : '#000000',
+});
---
+++ photoshop/placeSymbol/placeSymbol.js
@@ -0,0 +1,59 @@
+// tested by Adobe Photoshop CC 2017
+
+
+function getLayerSize(layer) {
+    var lengthWidth = layer.bounds[2]-layer.bounds[0];
+    var lengthHeight = layer.bounds[3]-layer.bounds[1];
+    var width = layer.bounds[3]-layer.bounds[1];
+    var height = layer.bounds[2]-layer.bounds[0];
+
+    lengthWidth = lengthWidth.toString().replace(' px', '');
+    lengthHeight = lengthHeight.toString().replace(' px', '');
+    width = width.toString().replace(' px', '');
+    height = height.toString().replace(' px', '');
+
+    return Array(height, width); // 거꾸로 되어있음;; 귀찮아..
+}
+
+function MoveLayerTo(fLayer,fX,fY) {
+
+  var Position = fLayer.bounds;
+
+  Position[0] = fX - Position[0];
+  Position[1] = fY - Position[1];
+
+  fLayer.translate(-Position[0],-Position[1]);
+}
+
+
+// set original document
+var originalDocument = activeDocument;
+
+// set symbol file
+var fileRef = File('/Users/redgoose/Dropbox/Assets/images/Redgoose-mini.png');
+var doc = open(fileRef);
+doc.changeMode(ChangeMode.RGB);
+doc.bitsPerChannel = BitsPerChannelType.EIGHT;
+doc.artLayers.add();
+doc.mergeVisibleLayers();
+
+// rename layer; duplicate to new document
+var layer = doc.activeLayer;
+layer.name = 'symbol';
+layer.duplicate(originalDocument, ElementPlacement.PLACEATBEGINNING);
+
+// close imported document
+doc.close(SaveOptions.DONOTSAVECHANGES);
+
+
+// move symbol
+var symbolLayer = activeDocument.artLayers.getByName('symbol');
+var layerSize = getLayerSize(symbolLayer);
+MoveLayerTo(
+    symbolLayer,
+    (Number(originalDocument.width) - layerSize[0] - 10),
+    (Number(originalDocument.height) - layerSize[1] - 10)
+);
+
+// change opacity
+symbolLayer.opacity = 50;
